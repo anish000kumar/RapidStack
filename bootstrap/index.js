f1557b1b4d92dfd01da03b987122fd857573cacb
Anish Kumar <akmarhw@amazon.com>
2019-02-28 16:00:37 +0530

    fixed issues

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -2,20 +2,14 @@ const { Router } = require('express');
 const registerApps = require('@app');
 const config = require('@config');
 const log = require('@helpers/log');
-const path = require('path');
-const filepath = path.join(__dirname, '@root/routes.generated.txt');
-const printRoutes = require('express-print-routes');
 
 function initApp({ app, db }) {
   const context = { app, db };
 
-  app.get('/', function(req, res) {
-    res.json(app._router.stack);
-  });
-
-  app.use('/api', function() {
-    registerApps(Router(), context);
-  });
+  // mount api
+  const api = Router();
+  registerApps(api, context);
+  app.use('/api', api);
 
   // start API server
   app.server.listen(process.env.PORT || config.port, () => {
@@ -25,7 +19,6 @@ function initApp({ app, db }) {
   });
 
   //printRoutes
-  if (process.env.NODE_ENV === 'development') printRoutes(app, filepath);
 }
 
 module.exports = initApp;
